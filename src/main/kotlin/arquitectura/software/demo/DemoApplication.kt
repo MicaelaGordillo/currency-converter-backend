c2137b10ac5d3daff1fc24ef5b4d002e5e747f09
MicaelaGordillo <micaa.gordilloa@gmail.com>
2023-04-13 10:37:58 -0400

    Connect to configuration server

--- src/main/kotlin/arquitectura/software/demo/DemoApplication.kt
+++ src/main/kotlin/arquitectura/software/demo/DemoApplication.kt
@@ -2,12 +2,14 @@ package arquitectura.software.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient
-//import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.context.annotation.Configuration
 
 @SpringBootApplication
-//@EnableDiscoveryClient
-//@EnableFeignClients
+@Configuration
+@EnableDiscoveryClient
+@EnableFeignClients
 class DemoApplication
 
 fun main(args: Array<String>) {
